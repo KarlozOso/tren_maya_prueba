9d7f7e12a564dc5de7e1f59c14d2fa7bffb67a01
KarlozOso <karlozoso90@gmail.com>
2023-08-08 04:17:17 -0600

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar"
+import { Route, Routes } from "react-router-dom"
+import './App.css'
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Service from "./routes/Service";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+        <Route path="/Service" element={<Service/>}/>
+      </Routes>
+        <Navbar/>
     </div>
   );
 }
-
-export default App;
