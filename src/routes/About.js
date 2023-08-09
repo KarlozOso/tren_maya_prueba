a8261639f75cbde832129c754b2f3cfcb3892252
KarlozOso <karlozoso90@gmail.com>
2023-08-09 03:51:04 -0600

    night changes

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,13 +1,16 @@
 import Navbar from "../components/Navbar"
+import Hero from "../components/Hero";
 
 function About (){
     return(
         <>
-          <Navbar/>
-        <h1>
-            This is About
-        </h1>
-        </>
+        <Navbar />
+        <Hero 
+          cName='hero-mid'
+          heroImg='https://images.unsplash.com/photo-1541427468627-a89a96e5ca1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+      title='About'
+          btnClass='hide'/>
+      </>
     )
 }
 
